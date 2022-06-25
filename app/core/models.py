8e64a4dfdd643e90213b70d84471c20e2abac651
Nahuelement <nahuel.perugi@gmail.com>
2022-06-25 15:29:58 -0400

    admin stting

--- app/core/models.py
+++ app/core/models.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import (
 )
 
 class UserManager(BaseUserManager):
+
     """Manager for users."""
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
@@ -39,4 +40,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    
